d82586bb4ef825642d9c6f05b93583045886e3f3
Marios Horacio Georgiades <96368918+MariosGeorgiades@users.noreply.github.com>
2026-02-19 18:43:07 +0200

    delete old files

--- A1_Relay_Deig.py
+++
@@ -1,7 +0,0 @@
-x = int(input())
-k= x*5
-m= x*3
-ki= x*2
-print(k)
-print(m)
-print(ki)
--- A2_Relay_Deig (3).py
+++
@@ -1,6 +0,0 @@
-x = float(input())
-print("%.0f"% (x//0.5))
-if x%0.5==0:
-    print(0)
-else:
-    print("%.2f"% (x%0.5))
